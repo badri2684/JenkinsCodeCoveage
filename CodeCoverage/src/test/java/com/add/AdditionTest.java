359729cfa15ed9e071136f967dd31120bacf9eb5
badri2684 <badri2526@gmail.com>
2016-12-13 19:28:14 +1100

    Add files via upload

---
+++ CodeCoverage/src/test/java/com/add/AdditionTest.java
@@ -0,0 +1,14 @@
+package com.add;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdditionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
